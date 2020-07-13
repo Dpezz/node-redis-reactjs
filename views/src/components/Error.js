4f6aa118c4518265a0e6dd7fffb39968307d1db9
Daniel Jara P <djara@corebox.cl>
2020-07-13 15:39:19 -0400

    add back button

--- views/src/components/Error.js
+++ views/src/components/Error.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Error() {
 	return (
 		<div>
 			<h1>Page not found!</h1>
+			<Link to="/">volver</Link>
 		</div>
 	);
 }
